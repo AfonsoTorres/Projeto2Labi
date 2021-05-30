d89b4e417f6253410d8051820ff2f0dc7c337cd9
AfonsoTorres <79586098+AfonsoTorres@users.noreply.github.com>
2021-05-30 21:48:22 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -8,9 +8,12 @@ import random
 from common_comm import send_dict, recv_dict, sendrecv_dict
 from Crypto.Cipher import AES
 
+# Dicionário com a informação relativa aos clientes
+gammers = {"socket":[],"cipher":[],"guess":[],"max_attempts":[],"attempts":[]}
+
 def main ():
     tcp_s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-    tcp_s.bind (("127.0.0.2", 1244))
+    tcp_s.bind (("127.0.0.1", 1244))
 
     tcp_s.listen ()
 
@@ -30,9 +33,17 @@ def main ():
     send_dict(client_s,msg)
 
     request = recv_dict (client_s)
-    user = base64.b64decode (request['client_id'])
+    user = base64.b64decode (request['client_id'])  
     user = cipher.decrypt (user)
     user = (str (user, 'utf8'))
+
+    if str(user) in gammers:
+        
+    else:
+        gammers["User"] = str(user)
+        print(gammers)
+
+    #gammers = {"socket":[],"cipher":[],"guess":[],"max_attempts":[],"attempts":[]}
 
     with open('report.csv', 'r', newline='') as file:
         nuser = 0
@@ -59,22 +70,101 @@ def main ():
 
     # { "op": "START", "status": False, "error": "Cliente existente" }
    # { "op": "START", "status": True, "max_attempts": nº máximo de jogadas }
-
+    ntrys = 0
     while 1:
+        print(secret_number)
+        print(ntrys)
         request = recv_dict (client_s)
-        data = base64.b64decode (request['value'])
-        data = cipher.decrypt (data)
-        data = int (str (data, 'utf8'))
-        print ("SERVER - Valor Recebido %d" % (data))
+        print(request)
 
-        data = data * 10
+        op = request['op'] 
 
-        print ("SERVER - Valor Enviado %d" % (data))
-        data = cipher.encrypt (bytes("%16d" % (data), 'utf8'))
-        data_tosend = str (base64.b64encode (data), 'utf8')
-        response = { 'value': data_tosend }
-        response = send_dict (client_s, response)
+        if op == "QUIT":
+            #{ "op": "QUIT", "status": True }
+            response = { 'op' : "QUIT",
+                "status" : True
+            }
+            send_dict(client_s,response)
+        elif op == "STOP":
+            attempts = request['attempts']
+            #attempts = cipher.decrypt (attempts)
+            #attempts = int (str (attempts, 'utf8'))
 
+            number = request['number']
+            #number = cipher.decrypt (number)
+            #number = int (str (number, 'utf8'))
+
+            if int(ntrys) == int(attempts):
+                if int(number) == int(secret_number):
+                    print("SUCCESS")
+                    number_secret = cipher.encrypt (bytes("%16d" % (secret_number), 'utf8'))
+                    number_secret_tosend = str (base64.b64encode (number_secret), 'utf8')
+                    response = { "op": "STOP", "status": True, "guess": number_secret_tosend }
+                    send_dict(client_s,response)
+                else:
+                    print("FAILURE")
+                    number_secret = cipher.encrypt (bytes("%16d" % (secret_number), 'utf8'))
+                    number_secret_tosend = str (base64.b64encode (number_secret), 'utf8')
+                    response = { "op": "STOP", "status": True, "guess": number_secret_tosend }
+                    send_dict(client_s,response)
+            else:
+                print(ntrys)
+                print(attempts)
+                print("FAILURE")
+                number_secret = cipher.encrypt (bytes("%16d" % (secret_number), 'utf8'))
+                number_secret_tosend = str (base64.b64encode (number_secret), 'utf8')
+                response = { "op": "STOP", "status": True, "guess": number_secret_tosend }
+                send_dict(client_s,response)
+        elif op == "GUESS":
+            #{ "op": "GUESS", "status": True, "result": "smaller"/"larger"/"equals" }
+            guessNum = -1
+            if 'number' in request.keys():
+                ntrys += 1
+                guessNum = base64.b64decode (request['number'])
+                guessNum = cipher.decrypt (guessNum)
+                guessNum = int (str (guessNum, 'utf8'))
+
+                if guessNum < secret_number:
+                    response = { 'op' : "GUESS",
+                    "status" : True,
+                    'result' : "smaller"
+                    }
+                    send_dict(client_s,response)
+                    print("send")
+
+                if guessNum > secret_number:
+                    response = { 'op' : "GUESS",
+                        "status" : True,
+                        'result' : "larger"
+                    }
+                    send_dict(client_s,response)
+
+                if guessNum == secret_number:
+                    response = { 'op' : "GUESS",
+                        "status" : True,
+                        'result' : "equals"
+                    }
+                    send_dict(client_s,response)
+            else:
+                print("Waiting for jogada")
+            
+        #if op == ""
+            #data = base64.b64decode (request['value'])
+            #data = cipher.decrypt (data)
+            #data = int (str (data, 'utf8'))
+            #print ("SERVER - Valor Recebido %d" % (data))
+
+            #data = data * 10
+
+            #print ("SERVER - Valor Enviado %d" % (data))
+            #data = cipher.encrypt (bytes("%16d" % (data), 'utf8'))
+            #data_tosend = str (base64.b64encode (data), 'utf8')
+            #response = { 'value': data_tosend }
+            #response = send_dict (client_s, response)
+        else:
+            print("Waiting for jogada")
+            print(response)
+            send_dict(client_s,response)
     client_s.close ()
     tcp_s.close ()
 main ()
